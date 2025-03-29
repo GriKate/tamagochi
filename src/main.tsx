967188a81d81118c3915c5dbda576ec10d66b2cc
Kate Grigoryeva <kate23april@yandex.ru>
2025-03-29 21:40:42 +0300

    add basic game

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
+
+// OLD
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.tsx'
+// import { BrowserRouter } from 'react-router-dom'
+
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </StrictMode>,
+// )
